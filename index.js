8ed2159627d805e37904513e6c5868c54bfd725f
Maggie Dalke <cdalke4@msn.com>
2020-12-02 11:49:04 -0600

    commiting json package and json package-lock

---
+++ index.js
@@ -0,0 +1,9 @@
+const dayjs = require('dayjs');
+
+console.log(dayjs().format('D MMMM YYYY')); // display
+
+// dayjs().set('month', 3).month() // get & set
+
+// dayjs().add(1, 'year') // manipulate
+
+// dayjs().isBefore(dayjs()) // query
