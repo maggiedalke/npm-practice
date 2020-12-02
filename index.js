a7bb56547c66984e5a4a4810e281d3f7137063e6
Maggie Dalke <cdalke4@msn.com>
2020-12-02 11:52:09 -0600

    adding chalk npm

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const dayjs = require('dayjs');
 
-console.log(dayjs().format('D MMMM YYYY')); // display
-
-// dayjs().set('month', 3).month() // get & set
-
-// dayjs().add(1, 'year') // manipulate
-
-// dayjs().isBefore(dayjs()) // query
